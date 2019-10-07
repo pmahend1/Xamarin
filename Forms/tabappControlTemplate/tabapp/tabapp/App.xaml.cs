6793ff0a479671bf2860d5431dfb81792c03dca9
Prateek <prateek.mahendrakar@gmail.com>
2019-10-07 14:00:36 -0400

    Tabapp controltemplate added

---
+++ Forms/tabappControlTemplate/tabapp/tabapp/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using tabapp.Services;
+using tabapp.Views;
+
+namespace tabapp
+{
+    public partial class App : Application
+    {
+        public static NavigationPage Navigation = null;
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            Navigation = new NavigationPage(new MainPage());
+            Application.Current.MainPage = Navigation;
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+        // Called by the back button in our header/navigation bar.
+        public async void OnBackButtonPressed(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
---
+++ Forms/tabappControlTemplate/tabapp/tabapp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using tabapp.Models;
+
+namespace tabapp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
